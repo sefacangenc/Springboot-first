0b6616c9a46a5b5a9fbc6b9a7bbabf549359fed0
sefa can  genc <sefacan19.sg@gmail.com>
2023-08-03 21:58:46 +0300

    fixing hibernate bug

--- src/main/java/com/example/Springbootfirst/controller/StudentController.java
+++ src/main/java/com/example/Springbootfirst/controller/StudentController.java
@@ -1,4 +1,26 @@
 package com.example.Springbootfirst.controller;
 
+import com.example.Springbootfirst.model.Student;
+import com.example.Springbootfirst.service.StudentService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/student")
 public class StudentController {
+    @Autowired
+    private StudentService studentService;
+
+    @PostMapping("/add")
+    public String add(@RequestBody Student student){
+        studentService.saveStudent(student);
+        return "news student added";
+    }
+
+    @GetMapping("/getAll")
+    public List<Student> list(){
+        return studentService.getAllStudents();
+    }
 }
